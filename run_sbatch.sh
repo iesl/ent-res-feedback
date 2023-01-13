3e4e4a94683fc734cac8fb687efc2236cbb466f0
Dhruv Agarwal <dhruv.agarwal17@gmail.com>
2023-01-13 04:20:01 +0000

    Fix run_sbatch

--- run_sbatch.sh
+++ run_sbatch.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -e
 echo CUDA_VISIBLE_DEVICES ${CUDA_VISIBLE_DEVICES}
 eval "$(conda shell.bash hook)"
+export PYTHONPATH=$(pwd):$PYTHONPATH;
 conda deactivate
 conda activate s2and
 python "$@"
